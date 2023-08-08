25c023ee7bb83392045105af1731ca63efe9f2e4
Mariana M.C <marianamoioli@gmail.com>
2023-08-08 10:17:23 -0300

    update

--- src/containers/Detail/index.jsx
+++ src/containers/Detail/index.jsx
@@ -1,58 +1,58 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+
+import {
+  getMovieById,
+  getMovieCredits,
+  getMovieSimilar,
+  getMovieVideos
+} from '../../services/getData'
 import { getImages } from '../../utils/getImages'
 import { Background, Container, Cover } from './styles'
 
-import {
-    getMovieById,
-    getMovieCredits,
-    getMovieSimilar,
-    getMovieVideos
-} from '../../services/getData'
-
 function Detail() {
-    const { id } = useParams()
-    const [movie, setMovie] = useState()
-    const [movieVideos, setMovieVideos] = useState()
-    const [movieCredits, setMovieCredits] = useState()
-    const [movieSimilar, setMovieSimilar] = useState()
+  const { id } = useParams()
+  const [movie, setMovie] = useState()
+  const [movieVideos, setMovieVideos] = useState()
+  const [movieCredits, setMovieCredits] = useState()
+  const [movieSimilar, setMovieSimilar] = useState()
 
-    useEffect(() => {
-        async function getAllData() {
-            Promise.all([
-                getMovieById(id),
-                getMovieVideos(id),
-                getMovieCredits(id),
-                getMovieSimilar(id),
-            ])
-                .then(([movie, videos, credits, similar]) => {
-                    console.log({ movie, videos, credits, similar })
-                    setMovie(movie)
-                    setMovieVideos(videos)
-                    setMovieCredits(credits)
-                    setMovieSimilar(similar)
-                })
-                .catch(error => console.error(error))
-        }
+  useEffect(() => {
+    async function getAllData() {
+      Promise.all([
+        getMovieById(id),
+        getMovieVideos(id),
+        getMovieCredits(id),
+        getMovieSimilar(id)
+      ])
+        .then(([movie, videos, credits, similar]) => {
+          console.log({ movie, videos, credits, similar })
+          setMovie(movie)
+          setMovieVideos(videos)
+          setMovieCredits(credits)
+          setMovieSimilar(similar)
+        })
+        .catch((error) => console.error(error))
+    }
 
-        getAllData()
-    }, [])
+    getAllData()
+  }, [])
 
-    return (
+  return (
+    <>
+      {movie && (
         <>
-            {movie && (
-                <>  
-                    <Background image={getImages(movie.backdrop_path)} />
-                    <Container>
-                        <Cover>
-                            <img src={getImages(movie.poster_path)} />
-                        </Cover>
-                        <div>Detalhe</div>
-                    </Container>
-                </>
-            )}
+          <Background image={getImages(movie.backdrop_path)} />
+          <Container>
+            <Cover>
+              <img src={getImages(movie.poster_path)} />
+            </Cover>
+            <div>Detalhe</div>
+          </Container>
         </>
-    )
+      )}
+    </>
+  )
 }
 
 export default Detail
