ca1a56c2af5c7cda6f9d7e59e3689467437a540b
Mariana M.C <marianamoioli@gmail.com>
2023-08-17 10:19:23 -0300

    Update

--- src/components/Credits/styles.js
+++ src/components/Credits/styles.js
@@ -1,5 +1,26 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  margin-top: 30px;
+  gap: 10px;
 
-export const Title = styled.h4``
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+
+  p {
+    color: #fff;
+  }
+
+  img {
+    height: 200px;
+  }
+`
+
+export const Title = styled.h4`
+  color: #fff;
+  font-size: 28px;
+  font-weight: 700;
+`
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,53 +1,53 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    min-height: 100px;
-    z-index: 99;
-    position: fixed;
-    top: 0;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 10px 50px;
-    background-color: ${props =>
-        props.changeBackground ? '#000' : 'transparent'};
-    transition: background-color 0.6s ease-in-out;
+  min-height: 100px;
+  z-index: 99;
+  position: fixed;
+  top: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 50px;
+  background-color: ${(props) =>
+    props.changeBackground ? '#000' : 'transparent'};
+  transition: background-color 0.6s ease-in-out;
 
-    img {
-        width: 25%;
-    }
+  img {
+    width: 25%;
+  }
 `
 
 export const Menu = styled.ul`
-    display: flex;
-    list-style: none;
-    gap: 50px;
+  display: flex;
+  list-style: none;
+  gap: 50px;
 `
 
 export const Li = styled.li`
-    font-weight: 600;
-    cursor: pointer;
-    font-size: 28px;
-    position: relative;
+  font-weight: 600;
+  cursor: pointer;
+  font-size: 28px;
+  position: relative;
 
-    a {
-        text-decoration: none;
-        color: #fff;
-    }
+  a {
+    text-decoration: none;
+    color: #fff;
+  }
 
-    &::after {
-        content: '';
-        height: 3px;
-        width: ${props => (props.isActive ? '100%' : 0)};
-        background-color: #189b20;
-        position: absolute;
-        bottom: -10px;
-        left: 50%;
-        transform: translateX(-50%);
-        transition: width 0.5s ease-in-out;
-    }
+  &::after {
+    content: '';
+    height: 3px;
+    width: ${(props) => (props.isActive ? '100%' : 0)};
+    background-color: #189b20;
+    position: absolute;
+    bottom: -10px;
+    left: 50%;
+    transform: translateX(-50%);
+    transition: width 0.5s ease-in-out;
+  }
 
-    &:hover::after {
-        width: 100%;
-    }
+  &:hover::after {
+    width: 100%;
+  }
 `
